fec9a2c0007ea9f556a3f50045d8177288430747
nathan_tsao <nathan_tsao@paradise-soft.com.tw>
2022-04-22 13:03:20 +0800

    add Bidirectional calculator

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -70,6 +70,34 @@ func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAvera
 	}
 }
 
+func (*server) FindMaximum(stream calculatorpb.CalculatorService_FindMaximumServer) error {
+	fmt.Println("Received FindMaximum RPC")
+	maximum := int32(0)
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+
+		}
+
+		number := req.GetNumber()
+		if number > maximum {
+			maximum = number
+			err = stream.Send(&calculatorpb.FindMaximumResponse{
+				Maximum: maximum,
+			})
+
+			if err != nil {
+				log.Fatalf("Error while sending data to client: %v", err)
+			}
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Calculator Server")
 
