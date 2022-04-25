6177de540b1841fb8218267102f37760d481e98c
nathan_tsao <nathan_tsao@paradise-soft.com.tw>
2022-04-25 14:38:39 +0800

    add insert blog

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/wolftsao/grpc-go-course/blog/blogpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println("Blog Client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("couldn't not connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	fmt.Println("Creating the blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Nathan",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+
+	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+
+	fmt.Printf("Blog has been created: %v\n", res)
+}
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -13,6 +13,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var collection *mongo.Collection
@@ -26,6 +28,41 @@ type blogItem struct {
 	AuthorID string             `bson:"author_id"`
 	Content  string             `bson:"content"`
 	Title    string             `bson:"title"`
+}
+
+func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	fmt.Println("Create blog request")
+	blog := req.GetBlog()
+	data := blogItem{
+		AuthorID: blog.GetAuthorId(),
+		Title:    blog.GetTitle(),
+		Content:  blog.GetContent(),
+	}
+
+	res, err := collection.InsertOne(ctx, data)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Internal error: %v", err),
+		)
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Cannot convert to OID: %v", err),
+		)
+	}
+
+	return &blogpb.CreateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AuthorId: blog.GetAuthorId(),
+			Title:    blog.GetTitle(),
+			Content:  blog.GetContent(),
+		},
+	}, nil
 }
 
 func main() {
