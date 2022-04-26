4cee830cb7fe06c8f61b0d14ebff4268d3c29d59
nathan_tsao <nathan_tsao@paradise-soft.com.tw>
2022-04-26 08:54:15 +0800

    add reflection

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -15,6 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -226,6 +227,7 @@ func main() {
 
 	s := grpc.NewServer(opts...)
 	blogpb.RegisterBlogServiceServer(s, &server{})
+	reflection.Register(s)
 
 	go func() {
 		fmt.Println("Starting Server...")
